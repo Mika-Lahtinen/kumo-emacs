4fccde8baf286f72695d9ba3cb1745d44370d5dc
Kurtis Moxley <image1224@outlook.com>
2023-06-30 16:28:16 +0800

    Change settings of LaTeX.

--- site-lisp/config/lang/latex.el
+++ site-lisp/config/lang/latex.el
@@ -1,5 +1,14 @@
 ;; (use-package auctex
 ;;     :ensure t)
-(require 'latex)
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
+(add-hook 'LaTeX-mode-hook
+(lambda()
+(add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+(setq TeX-command-default "XeLaTeX")))
 
 (provide 'lang/LaTeX)
