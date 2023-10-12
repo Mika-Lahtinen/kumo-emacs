facb21fdccb825611c41bfa78548dcb3d40eee6e
Kurtis Moxley <image1224@outlook.com>
2023-10-12 21:46:03 +0800

    Add todo routines

---
+++ lisp/tools/edit.el
@@ -0,0 +1,16 @@
+;; (use-package evil
+;;     :ensure t
+;;     :init
+;;     (progn 
+;;         (setq evil-undo-system 'undo-tree)
+;;         (setq evil-want-keybinding nil))
+;;     :config
+;;     (evil-mode 1))
+
+;; (use-package evil-collection
+;;     :after evil
+;;     :ensure t
+;;     :config
+;;     (evil-collection-init))
+
+(provide 'tools/edit)
