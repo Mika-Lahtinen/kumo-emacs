a320b243115ed8f092138390b93cffdac25e6801
Kurtis Moxley <image1224@outlook.com>
2024-01-29 16:50:36 +0800

    Update LSP settings.

--- lisp/tools/lsp.el
+++ lisp/tools/lsp.el
@@ -1,34 +1,34 @@
 
 ;; YASnippets settings
-;;(use-package yasnippet
-;;    :load-path "packages/yasnippet"
-;;    :hook
-;;    (prog-mode . yas-minor-mode)
-;;    :config
-;;    (yas-reload-all)
-;;    (defun company-mode/backend-with-yas (backend)
-;;    (if (and (listp backend) (member 'company-yasnippet backend))
-;;      backend
-;;        (append (if (consp backend) backend (list backend))
-;;              '(:with company-yasnippet))))
-;;    (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-;;    ;; unbind <TAB> completion
-;;    (define-key yas-minor-mode-map [(tab)]        nil)
-;;    (define-key yas-minor-mode-map (kbd "TAB")    nil)
-;;    (define-key yas-minor-mode-map (kbd "<tab>")  nil)
-;;    :bind
-;;    (:map yas-minor-mode-map
-;;         ("S-<tab>" . yas-expand))
-;;    )
-;;
-;;(use-package yasnippet-snippets
-;;    :load-path "packages/yasnippet-snippets"
-;;    :ensure t
-;;    :after yasnippet)
+(use-package yasnippet
+             :load-path "packages/yasnippet"
+             :hook
+             (prog-mode . yas-minor-mode)
+             :config
+             (yas-reload-all)
+             (defun company-mode/backend-with-yas (backend)
+               (if (and (listp backend) (member 'company-yasnippet backend))
+                 backend
+                 (append (if (consp backend) backend (list backend))
+                         '(:with company-yasnippet))))
+             (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+             ;; unbind <TAB> completion
+             (define-key yas-minor-mode-map [(tab)]        nil)
+             (define-key yas-minor-mode-map (kbd "TAB")    nil)
+             (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+             :bind
+             (:map yas-minor-mode-map
+                   ("S-<tab>" . yas-expand))
+             )
+
+(use-package yasnippet-snippets
+             :load-path "packages/yasnippet-snippets"
+             :ensure t
+             :after yasnippet)
 
 ;; Eglot settings
 (use-package eglot
-            :load-path "packages/eglot"
+             :load-path "packages/eglot"
              :hook (prog-mode . eglot-ensure)
              :bind ("C-c e f" . eglot-format))
 
