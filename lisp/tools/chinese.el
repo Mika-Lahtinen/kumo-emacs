e82f178e8f58cf3baacf40acbe7b7ef252eff9c8
Kurtis Moxley <image1224@outlook.com>
2024-01-30 09:35:52 +0800

    Update Chinese input settings.

--- lisp/tools/chinese.el
+++ lisp/tools/chinese.el
@@ -5,14 +5,13 @@
              ("C-\\" . toggle-input-method)
              :init
              (require 'pyim)
-             (require 'pyim-basedict)
-             (pyim-basedict-enable)
              (setq default-input-method "pyim")
              (setq pyim-cloudim 'baidu)
              (pyim-default-scheme 'microsoft-shuangpin))
 
 (use-package pyim-tsinghua-dict
              :load-path "packages/pyim-tsinghua-dict"
+             :after pyim
              :config
              (require 'pyim-tsinghua-dict)
              (pyim-tsinghua-dict-enable))
