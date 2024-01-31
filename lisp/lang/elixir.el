4c1224da8d8da7ac83b185dc319d94f45aaa0f2a
Kurtis Moxley <image1224@outlook.com>
2024-01-31 13:37:44 +0800

    Update elixir settings.

--- lisp/lang/elixir.el
+++ lisp/lang/elixir.el
@@ -1,6 +1,8 @@
 (use-package elixir-mode
     :load-path "packages/elixir-mode"
+    :autoload (elixir-mode)
     :mode "\\.elixir2\\'"
+    :hook (elixir-mode . eglot-ensure)
     )
 
 (use-package exunit
