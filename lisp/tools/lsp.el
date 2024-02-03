b13e067ef835e0e4bc10e58615164b8304283167
Kurtis Moxley <image1224@outlook.com>
2024-02-04 00:21:17 +0800

    Add flymake support.

--- lisp/tools/lsp.el
+++ lisp/tools/lsp.el
@@ -1,3 +1,9 @@
+(use-package flymake
+  :hook (prog-mode . flymake-mode)
+  :config
+  (global-set-key (kbd "M-n") #'flymake-goto-next-error)
+  (global-set-key (kbd "M-p") #'flymake-goto-prev-error))
+
 ;; Eglot settings
 (use-package eglot
              :load-path "packages/eglot"
