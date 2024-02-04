8625638f69230d02f75d1d4c3173e2ef93a0dc07
Kurtis Moxley <image1224@outlook.com>
2024-02-04 13:22:00 +0800

    Update elixir settings.

--- lisp/lang/elixir.el
+++ lisp/lang/elixir.el
@@ -1,13 +1,10 @@
-;; (use-package elixir-mode
-;;              :load-path "packages/elixir-mode"
-;;              :mode ("\.ex$" . elixir-mode)
-;;              :config
-;;              (require 'elixir-mode))
+(use-package elixir-mode
+             :load-path "packages/elixir-mode")
 
-(add-to-list 'load-path
-             (concat user-emacs-directory
-                     (convert-standard-filename "packages/elixir-mode/")))
-(require 'elixir-mode)
+;; (add-to-list 'load-path
+;;              (concat user-emacs-directory
+;;                      (convert-standard-filename "packages/elixir-mode/")))
+;; (require 'elixir-mode)
 
 (use-package exunit
              :load-path "packages/exunit"
