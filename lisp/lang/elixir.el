1bc1a521af368ce2ba14d73396061428b0a1273c
Kurtis Moxley <image1224@outlook.com>
2024-02-04 14:03:08 +0800

    Update elixir settings.

--- lisp/lang/elixir.el
+++ lisp/lang/elixir.el
@@ -1,10 +1,16 @@
 (use-package elixir-mode
-             :load-path "packages/elixir-mode")
+             :load-path "packages/elixir-mode"
+             :bind (:map elixir-mode-map
+                         ("C-c C-f" . elixir-format)))
 
 (require 'elixir-mode)
 
 (use-package exunit
              :load-path "packages/exunit"
              :hook (elixir-mode . exunit-mode))
+
+(use-package mix
+             :load-path "packages/mix"
+             :hook (elixir-mode . mix-minor-mode))
 
 (provide 'lang/elixir)
