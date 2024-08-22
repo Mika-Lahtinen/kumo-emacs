0a7fd3c1a84f57174747a0eb53838d863d9d4997
Kurtis Moxley <image1224@outlook.com>
2024-08-22 18:42:14 +0800

    Fix bug

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
 
 ;;; Basic settings for all Emacs.
 (add-to-list 'load-path
-             (expand-file-name(concat user-emacs-directory "lisp/base/")))
+             (expand-file-name (concat user-emacs-directory "lisp/base/")))
 ;; (require 'init-package-source)
 ;; (require 'init-base-package)
 (require 'init-basic)
