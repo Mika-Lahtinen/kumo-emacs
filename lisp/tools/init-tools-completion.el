5539fe2430c986cc4b2a27a0c6b8fd52abb04eca
Kurtis Moxley <image1224@outlook.com>
2024-08-30 02:27:08 +0800

    Fix company bug.

--- lisp/tools/init-tools-completion.el
+++ lisp/tools/init-tools-completion.el
@@ -41,8 +41,8 @@
 (use-package company
   :ensure nil
   :load-path "packages/company/"
-  :hook
-  (after-init . global-company-mode)
+  :init
+  (global-company-mode)
   :config
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 1)
