221f89ced6a4e7ae5a2946640eb39fcd27b8f6d7
Kurtis Moxley <image1224@outlook.com>
2024-08-30 14:11:04 +0800

    Separate company settings.

---
+++ lisp/tools/init-tools-company.el
@@ -0,0 +1,28 @@
+;;; init-tools-company.el --- Configuration for Company package.
+; -*- lexical binding: t -*-
+;;; Commentary: 
+;; Confugurations for Company package.
+;;; Code:
+
+;;; Company: For auto completion when coding.
+(use-package company
+    :ensure nil
+    :load-path "packages/company/"
+    :hook
+    (after-init . global-company-mode)
+    :config
+    (setq company-idle-delay 0)
+    (setq company-minimum-prefix-length 1)
+    (setq company-selection-wrap-around t)
+    (setq company-tooltip-limit 10)
+    (setq company-tooltip-align-annotations t)
+    (setq company-dabbrev-downcase nil)
+    (setq company-dabbrev-ignore-case nil))
+
+(use-package company-box
+    :ensure nil
+    :load-path "packages/company-box"
+    :hook
+    (company-mode . company-box-mode))
+
+(provide 'init-tools-company)
--- lisp/tools/init-tools-completion.el
+++ lisp/tools/init-tools-completion.el
@@ -10,48 +10,32 @@
 
 ;;; Ivy + Counsel + Swiper
 (use-package counsel
-  :ensure nil
-  :load-path "packages/swiper/"
-  :hook
-  (after-init . counsel-mode))
+    :ensure nil
+    :load-path "packages/swiper/"
+    :hook
+    (after-init . counsel-mode))
 
 (use-package ivy
-  :ensure nil
-  :load-path "packages/swiper/"
-  :hook
-  (after-init . ivy-mode)
-  :bind
-  (
-    ("C-s" . 'swiper)
-    ("C-x b" . 'ivy-switch-buffer))
-  :config
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-  (setq ivy-height 10)
-  (setq ivy-wrap t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-re-builders-alist
-        '((t . ivy--regex-ignore-order))))
+    :ensure nil
+    :load-path "packages/swiper/"
+    :hook
+    (after-init . ivy-mode)
+    :bind
+    (
+      ("C-s" . 'swiper)
+      ("C-x b" . 'ivy-switch-buffer))
+    :config
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    (setq ivy-height 10)
+    (setq ivy-wrap t)
+    (setq ivy-count-format "(%d/%d) ")
+    (setq ivy-re-builders-alist
+          '((t . ivy--regex-ignore-order))))
 
 (use-package swiper
-  :ensure nil
-  :load-path "packages/swiper/")
-
-;;; Company: For auto completion when coding.
-(use-package company
-  :ensure nil
-  :load-path "packages/company/"
-  :hook
-  (after-init . global-company-mode)
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1)
-  (setq company-selection-wrap-around t)
-  (setq company-tooltip-limit 10)
-  (setq company-tooltip-align-annotations t)
-  (setq company-dabbrev-downcase nil)
-  (setq company-dabbrev-ignore-case nil))
-
+    :ensure nil
+    :load-path "packages/swiper/")
 
 ;;; Which-key: For displaying keybindings.
 
