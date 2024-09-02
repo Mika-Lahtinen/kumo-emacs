439b2558de57c9dc19f0b01a5105231069bbb0b8
Kurtis Moxley <image1224@outlook.com>
2024-09-02 16:46:32 +0800

    Completion set.

--- lisp/tools/init-tools-completion.el
+++ lisp/tools/init-tools-completion.el
@@ -3,10 +3,74 @@
 ;;; Commentary:
 ;; All the configurations for auto completion.
 ;; Currently we are using the following plugins:
-;; ivy, swiper, counsel, company
+;; vertico, orderless, marginalia, embark, consult, corfu
 ;; Additional plugins:
 ;; which-key, keycast
 ;;; Code:
+
+;;; Vertico
+(use-package vertico
+    :ensure nil
+    :load-path "packages/vertico/"
+    :hook
+    (after-init . vertico-mode)
+    :custom
+    (vertico-cycle t))
+
+;;; orderless
+(use-package orderless
+    :ensure nil
+    :load-path "packages/orderless/"
+    :custom
+    (completion-styles '(orderless basic))
+    (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;;; marginalia
+(use-package marginalia
+    :ensure nil
+    :load-path "packages/marginalia/"
+    :hook
+    (after-init . marginalia-mode))
+
+;;; consult
+(use-package consult
+    :ensure nil
+    :load-path "packages/consult/"
+    :bind
+    (("C-s" . consult-line)
+     ("C-x b" . consult-buffer)
+     ("C-x C-b" . consult-buffer)
+     ("C-x C-f" . consult-find)
+     ("C-x C-r" . consult-recent-file)))
+
+;;; embark
+(use-package embark
+    :ensure nil
+    :load-path "packages/embark/"
+    :bind
+    (("C-." . embark-act)
+     ("C-;" . embark-dwim)
+     ("C-h B" . embark-bindings))
+    :init
+    (setq prefix-help-command #'embark-prefix-help-command)
+    :config
+    (add-to-list 'embark-allow-quit 'embark-act))
+
+;;; corfu
+(use-package corfu
+    :ensure nil
+    :load-path "packages/corfu/"
+    :hook
+    (after-init . corfu-global-mode)
+    :custom
+    (corfu-cycle t)
+    (corfu-auto t)
+    (corfu-separator ?\s)
+    (corfu-quit-at-boundary t)
+    (corfu-quit-no-match t)
+    (corfu-preview-current nil)
+    (corfu-preselect-first nil)
+    (corfu-popupinfo-delay 0.5))
 
 ;;; Which-key: For displaying keybindings.
 
