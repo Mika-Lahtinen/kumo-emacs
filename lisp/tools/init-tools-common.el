bab765ca2ff322fb7014c13fc7aa1f68ae9fc805
Kurtis Moxley <image1224@outlook.com>
2025-05-24 09:08:01 +0800

    Update common settings.

--- lisp/tools/init-tools-common.el
+++ lisp/tools/init-tools-common.el
@@ -2,6 +2,8 @@
 ;; be moved until I'm sure where could be suitable to move.
 
 (use-package exec-path-from-shell
-    :ensure t)
+  :ensure t
+  :init
+  (exec-path-from-shell-initialize))
 
 (provide 'init-tools-common)
