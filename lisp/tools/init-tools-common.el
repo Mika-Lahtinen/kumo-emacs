715889e9296a5b5f70f57a61bf3b00a353e3da24
Kurtis Moxley <image1224@outlook.com>
2025-05-25 13:13:14 +0800

    Update common settings.

--- lisp/tools/init-tools-common.el
+++ lisp/tools/init-tools-common.el
@@ -1,9 +1,18 @@
+;;; init-tools-common --- Common tools
+;;; Commentary:
 ;; Common used packages by others, the following configuration wouldn't
 ;; be moved until I'm sure where could be suitable to move.
+;;; Code:
 
 (use-package exec-path-from-shell
   :ensure t
   :init
   (exec-path-from-shell-initialize))
 
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
 (provide 'init-tools-common)
+;;; init-tools-common.el ends here
