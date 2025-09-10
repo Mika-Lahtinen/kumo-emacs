8c1d67147aec85e67ece12994ff75a0ccf09aa91
Kurtis Moxley <image1224@outlook.com>
2025-09-10 13:31:11 +0800

    Change auto completion plugins.

--- lisp/tools/init-tools-completion.el
+++ lisp/tools/init-tools-completion.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 ;; All the configurations for auto completion.
 ;; Currently we are using the following plugins:
-;; Corfu, Embark, Marginalia, Orderless, company
+;; Corfu, Embark, Marginalia, Orderless, Vertico, company
 ;; Additional plugins:
 ;; which-key, keycast
 ;;; Code:
@@ -12,23 +12,30 @@
 (use-package corfu
              :ensure nil
              :load-path (plugin-load "corfu")
-             )
-(use-package embark)
-(use-package marginalia)
-(use-package orderless)
-;;; Company
-(use-package company
+             :init
+             (corfu-global-mode))
+
+(use-package embark
              :ensure nil
-             :load-path (plugin-load "company")
-             :hook
-             (after-init . global-company-mode)
-             :config
-             (setq company-idle-delay 0.3
-                   company-minimum-prefix-length 2
-                   company-show-numbers t
-                   company-selection-wrap-around t
-                   company-tooltip-align-annotations t)
-             )
+             :load-path (plugin-load "embark"))
+
+(use-package marginalia
+             :ensure nil
+             :load-path (plugin-load "marginalia"))
+
+(use-package orderless
+             :ensure nil
+             :load-path (plugin-load "orderless"))
+
+(use-package vertico
+             :ensure nil
+             :load-path (plugin-load "vertico")
+             :init
+             (vertico-mode))
+
+(use-package consult
+             :ensure nil
+             :load-path (plugin-load "consult"))
 
 (provide 'init-tools-completion)
 ;;; init-tools-completion.el ends here.
